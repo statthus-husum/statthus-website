3b7133e5314790b3273d9c6e6fa3a1fd61235763
Martin Jahr <martin.jahr@statthus-husum.de>
2025-11-26 22:12:37 +0100

    initial

---
+++ themes/logbook-hugo/assets/js/script.js
@@ -0,0 +1,129 @@
+(function () {
+  ("use strict");
+
+  // header search panel
+  const navbar_collapse = document.querySelector(".navbar-collapse");
+  const navbar_toggler = document.querySelector(".navbar-toggler");
+  const header_search_panel = document.querySelector(".header-search-panel");
+  const open_header_search_panel = document.querySelectorAll(
+    '[data-target="open-header-search-panel"]'
+  );
+  const close_header_search_panel = document.querySelectorAll(
+    '[data-target="close-header-search-panel"]'
+  );
+
+  if (header_search_panel) {
+    open_header_search_panel.forEach((item) => {
+      item.addEventListener("click", function () {
+        header_search_panel.classList.add("show");
+        navbar_collapse.classList.remove("show");
+        navbar_toggler.classList.add("collapsed");
+        navbar_toggler.setAttribute("aria-expanded", "false");
+        setTimeout(() => {
+          header_search_panel.querySelector("input").focus();
+        }, 100);
+      });
+    });
+    close_header_search_panel.forEach((item) => {
+      item.addEventListener("click", function () {
+        header_search_panel.classList.remove("show");
+      });
+    });
+  }
+
+  // back to top button
+  document.querySelector("#scrollTop")?.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  //post slider
+  let post_sliders = document.querySelectorAll(".post-slider");
+  if (post_sliders[0]) {
+    for (let index = 0; index < post_sliders.length; index++) {
+      new Swiper(post_sliders[index], {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        autoplay: {
+          delay: 5000,
+        },
+        navigation: {
+          nextEl: ".post-slider .swiper-button-next",
+          prevEl: ".post-slider .swiper-button-prev",
+        },
+      });
+    }
+  }
+
+  // featured post slider
+  new Swiper(".featured-post-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: ".featured-post-slider .swiper-button-next",
+      prevEl: ".featured-post-slider .swiper-button-prev",
+    },
+    pagination: {
+      el: ".featured-post-slider .swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // product Slider Pagination
+  let product_silder_pagination = new Swiper(
+    ".single-product-slider-pagination",
+    {
+      slidesPerView: "auto",
+    }
+  );
+
+  // product Slider
+  new Swiper(".single-product-slider", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    thumbs: {
+      swiper: product_silder_pagination,
+    },
+  });
+
+  const searchForm = header_search_panel.querySelector("form");
+  if (searchForm) {
+    searchForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      const searchQuery = this.querySelector('input[name="s"]').value.trim();
+      if (searchQuery) {
+        const searchUrl = `${this.action}?s=${encodeURIComponent(
+          searchQuery
+        )}&init=1`;
+        window.location.href = searchUrl;
+      }
+    });
+  }
+
+  if (window.location.pathname.includes("/search")) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const searchQuery = urlParams.get("s");
+    const isInitialSearch = urlParams.get("init");
+
+    if (searchQuery) {
+      const searchInput = document.getElementById("search-input");
+      if (searchInput) {
+        searchInput.value = searchQuery;
+
+        if (isInitialSearch) {
+          urlParams.delete("init");
+          const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+          history.replaceState(null, "", newUrl);
+
+          searchInput.dispatchEvent(new Event("input", { bubbles: true }));
+        }
+      }
+    }
+  }
+})();
